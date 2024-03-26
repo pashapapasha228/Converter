ff777ec12af4ff7db360be4b3a23aabe80fe97d9
pashapapasha228 <Luksash11@gmail.com>
2024-03-26 09:29:23 +0300

    Update ConvertHistoryController.java

--- src/main/java/by/java/converter/controller/ConvertHistoryController.java
+++ src/main/java/by/java/converter/controller/ConvertHistoryController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("api/v1/history")
+@RestController()
+@RequestMapping("api/v1/history")
 public class ConvertHistoryController {
     private final ConvertHistoryService convertHistoryService;
 
